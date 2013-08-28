b0a1c92e6b309beee69dfc23a7f6d453c85086f7
nippo <n@ippo.io>
2013-08-28 22:58:05 +0200

    Removed call to bbox

--- edigeo/views.py
+++ edigeo/views.py
@@ -3,8 +3,6 @@ from models import (Parcelle, EdigeoLieuDit,
 from django.http import HttpResponse
 from django.contrib.auth.decorators import user_passes_test
 from geoshortcuts.geojson import render_to_geojson
-from django.contrib.gis.geos import Polygon
-import ast
 from django.views.decorators.gzip import gzip_page
 
 
@@ -36,12 +34,6 @@ def bati(request):
 @gzip_page
 def parcel(request):
     qs = Parcelle.objects.all()
-    bbox = ast.literal_eval(
-        '(' + request.GET.get('bbox', '-160, -89, 160, 89') + ')')
-
-    polygon = Polygon.from_bbox(bbox)
-    polygon.set_srid(4326)
-    polygon.transform(900913)
     json = render_to_geojson(
         qs,
         projection=4326,
@@ -56,10 +48,8 @@ def parcel(request):
 @gzip_page
 def lieudit(request):
     qs = EdigeoLieuDit.objects.all()
-    bbox = ast.literal_eval(
-        "(" + request.GET.get('bbox', '(0, 180, 0, 180)') + ")")
     json = render_to_geojson(qs, projection=4326, properties=[
         ('gb_ident', 'gb_ident')],
-        extent=Polygon.from_bbox(bbox).set_srid(4326)
+        #extent=Polygon.from_bbox(bbox).set_srid(4326)
     )
     return HttpResponse(json, content_type=u'application/json')
